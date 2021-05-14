748fb31f3fe893fe72ea85797a4a5ac07b72481d
Aaron Lee <aaronlee@Aarons-MacBook-Pro.local>
2021-05-13 17:03:51 -0700

    hello world

--- asset_dashboard/asset_dashboard/urls.py
+++ asset_dashboard/asset_dashboard/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from dash import views
 
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
     path('dash/', include('dash.urls'))
 ]
